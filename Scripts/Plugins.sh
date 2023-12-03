7f45399f3c49bca06594938e55fd77a48745f6c0
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-03 09:34:15 +0800

    补充对vlmcsd的支持

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -33,6 +33,7 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-ser
 # svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite package/msd_lite
 svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
 #svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-qbittorrent package/luci-app-qbittorrent
+svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
 
 # 科学上网插件
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
@@ -78,6 +79,7 @@ svn co https://github.com/NueXini/NueXini_Packages/trunk/UnblockNeteaseMusic pac
 #svn co https://github.com/kenzok8/small-package/trunk/aria2 package/aria2
 #svn co https://github.com/kenzok8/small-package/trunk/ariang package/ariang
 #svn co https://github.com/kenzok8/small-package/trunk/qBittorrent-static package/qBittorrent-static
+svn co https://github.com/NueXini/NueXini_Packages/trunk/vlmcsd package/vlmcsd
 
 # Themes
 #git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
