33098696de2123791040e17f4dc133f00d35f998
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-08 13:55:03 +0800

    只给openwrt库增加redmi_ax6普通构架编译

--- Scripts/Plugins_openwrt.sh
+++ Scripts/Plugins_openwrt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
