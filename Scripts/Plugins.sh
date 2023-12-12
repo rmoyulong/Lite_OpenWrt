7d4490022e0c62d31e2554f5d31ea2fb1aeb527d
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-12 09:05:59 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -12,7 +12,7 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-openclash
 
 echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
 svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
