5b381f50974896f3772e26967003355831703bca
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-02 23:23:59 +0800

    更换为kiddin9订阅源

--- Scripts/Plugins_openwrt.sh
+++ Scripts/Plugins_openwrt.sh
@@ -19,13 +19,13 @@ echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
 
-rm -rf feeds/luci/modules/luci-base
-rm -rf feeds/luci/modules/luci-mod-status
-rm -rf feeds/packages/utils/coremark
-rm -rf package/emortal/default-settings
+#rm -rf feeds/luci/modules/luci-base
+#rm -rf feeds/luci/modules/luci-mod-status
+#rm -rf feeds/packages/utils/coremark
+#rm -rf package/emortal/default-settings
 
-svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-base feeds/luci/modules/luci-base
-svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
-svn export https://github.com/immortalwrt/packages/branches/master/utils/coremark package/new/coremark
-svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/default-settings package/emortal/default-settings
+#svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-base feeds/luci/modules/luci-base
+#svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
+#svn export https://github.com/immortalwrt/packages/branches/master/utils/coremark package/new/coremark
+#svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/default-settings package/emortal/default-settings
 # svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/utils/mhz package/utils/mhz
