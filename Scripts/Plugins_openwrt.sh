8b0c5cd5e44ea6ca391cbc0754843ba1eb832893
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-02 21:21:47 +0800

    修改插件名字，部分替换为ImmortalWrt库文件

--- Scripts/Plugins_openwrt.sh
+++ Scripts/Plugins_openwrt.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-homeproxy
+
 echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/luci-app-unblockmusic
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
