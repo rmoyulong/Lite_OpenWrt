d5761ed0a7797a78247114a8884ff4cda456ad1a
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-13 14:05:12 +0800

    Update Plugins_k2_L1.sh

--- Scripts/Plugins_k2_L1.sh
+++ Scripts/Plugins_k2_L1.sh
@@ -4,8 +4,6 @@
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 
-# 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 
 
