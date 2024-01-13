0b0a008a158e019d22de326ed346decda884f079
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-13 14:04:30 +0800

    Update Plugins_k2_L1.sh

--- Scripts/Plugins_k2_L1.sh
+++ Scripts/Plugins_k2_L1.sh
@@ -2,11 +2,10 @@
 
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+
 
