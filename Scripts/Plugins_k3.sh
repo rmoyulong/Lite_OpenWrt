47e832131cbbb0ca9fbce05f5bf016a87b313ce9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-04-01 09:16:53 +0800

    Phicomm_K3

--- Scripts/Plugins_k3.sh
+++ Scripts/Plugins_k3.sh
@@ -3,7 +3,6 @@
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git batman https://git.open-mesh.org/openwrt-feed-devel.git' >> feeds.conf.default
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
