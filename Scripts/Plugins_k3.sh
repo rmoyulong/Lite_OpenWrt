d9af7da22ecd4e046d197d63c0b91410e960f62e
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-04-01 11:37:20 +0800

    Phicomm_K3

--- Scripts/Plugins_k3.sh
+++ Scripts/Plugins_k3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#lede
+echo 'src-git packages https://github.com/coolsnowwolf/packages' > feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
+#src-git routing https://github.com/coolsnowwolf/routing
+echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-21.02' >> feeds.conf.default
+
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
@@ -16,7 +22,8 @@ echo '=========Replace k3screen drive plug OK!========='
 
 FIRMWARE='69027'
 echo '替换无线驱动'
-wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${FIRMWARE} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+echo 'https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${FIRMWARE}'
+wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
 
 
