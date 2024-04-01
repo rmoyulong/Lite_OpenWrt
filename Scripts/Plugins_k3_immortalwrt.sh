4bada14966886e8e8178d824bb3f08f4977f14e3
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-04-01 19:21:31 +0800

    immortalwrt_k3

--- Scripts/Plugins_k3_immortalwrt.sh
+++ Scripts/Plugins_k3_immortalwrt.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+  
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
@@ -19,6 +29,8 @@ echo '替换无线驱动'
 echo 'https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${{FIRMWARE}}'
 wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
+
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
 
 
 
