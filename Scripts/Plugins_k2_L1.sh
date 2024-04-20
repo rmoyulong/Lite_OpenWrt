1201ed6c8d8f44f98921544b7d0bba477a7c2975
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-04-20 12:59:44 +0800

    K2和优酷路由宝L1

--- Scripts/Plugins_k2_L1.sh
+++ Scripts/Plugins_k2_L1.sh
@@ -4,6 +4,18 @@
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
+git_sparse_clone master https://github.com/kiddin9/cups-bjnp cups-bjnp
+git_sparse_clone master https://github.com/kiddin9/cups cups
+git_sparse_clone master https://github.com/kiddin9/luci-app-cupsd cupsd
 
 
