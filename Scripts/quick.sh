25168bc3b35ad4d5d2907ba21b111aefff5dbb99
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-04-22 14:50:48 +0800

    更改ssrplus源

--- Scripts/quick.sh
+++ Scripts/quick.sh
@@ -36,8 +36,6 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages ariang
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-
-rm -rf small/luci-app-ssr-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
