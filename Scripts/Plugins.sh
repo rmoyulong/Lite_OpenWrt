a40e08ba193982268114c14873a8b930e2eaf239
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-12 19:03:10 +0800

    更换为kiddin9订阅源

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -14,3 +14,15 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 
 #echo 'src-git kenzo8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+  
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
