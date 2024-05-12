d25dacc704e9908f3e306c17bbc301dc34912b4a
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-12 19:02:49 +0800

    immortalwrt_k3

--- Scripts/Plugins_k3_immortalwrt.sh
+++ Scripts/Plugins_k3_immortalwrt.sh
@@ -10,6 +10,8 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
   
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
+
 # 科学上网插件
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
