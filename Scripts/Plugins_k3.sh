4155a60051f796b3d0ad2fbbfc516ee9e6e06599
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-12 19:02:22 +0800

    Phicomm_K3

--- Scripts/Plugins_k3.sh
+++ Scripts/Plugins_k3.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+  
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
+  
 #lede
 echo 'src-git packages https://github.com/coolsnowwolf/packages' > feeds.conf.default
 echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
