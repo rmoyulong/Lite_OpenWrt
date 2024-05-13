53d917f03e7c0d7b31ae033c879c78fc90898485
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-13 12:50:17 +0800

    immortalwrt自定义扩展源

--- Scripts/quick.sh
+++ Scripts/quick.sh
@@ -32,25 +32,28 @@ function git_sparse_clone() {
 }
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 ######################################################################################
 #echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
 ######################################################################################
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >> feeds.conf.default
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
 #######################################################################################
-#git_sparse_clone master https://github.com/fw876/helloworld helloworld
-#git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall openwrt-passwall
-#git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall2 openwrt-passwall2
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 #git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
+#git_sparse_clone main https://github.com/kenzok8/small-package ariang
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
-#git_sparse_clone main https://github.com/kenzok8/small-package ariang
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-vlmcsd
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
