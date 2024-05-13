6119426a177f79ef3ba18f158849ee4b26d18cab
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-13 17:21:01 +0800

    immortalwrt_k3

--- Scripts/Plugins_k3_immortalwrt.sh
+++ Scripts/Plugins_k3_immortalwrt.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# 移除要替换的包
+rm -rf feeds/packages/net/alist
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/packages/net/ariang
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-msd_lite
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+#rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-turboacc
+#rm -rf feeds/packages/net/shadowsocks-libev
+
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -10,12 +31,34 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+######################################################################################
+#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
+######################################################################################
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
+#######################################################################################
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede qca
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
-
-# 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
+#git_sparse_clone main https://github.com/kenzok8/small-package ariang
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
+#echo 'src-git my_luci https://github.com/rmoyulong/my_luci' >> feeds.conf.default
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
@@ -32,9 +75,3 @@ echo '替换无线驱动'
 echo 'https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${{FIRMWARE}}'
 wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
-
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-
-
-
-
