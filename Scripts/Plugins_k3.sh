8dfbbd28ddf67e190073edaff5253d7345d610a2
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-14 02:10:09 +0800

    Plugins_k3

--- Scripts/Plugins_k3.sh
+++ Scripts/Plugins_k3.sh
@@ -31,34 +31,6 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-######################################################################################
-#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
-######################################################################################
-#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
-#######################################################################################
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede qca
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
-#git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
-#git_sparse_clone main https://github.com/kenzok8/small-package ariang
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-maxminddb
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-kodexplorer
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-usb-printer
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
-#echo 'src-git my_luci https://github.com/rmoyulong/my_luci' >> feeds.conf.default
-  
 #lede
 echo 'src-git packages https://github.com/coolsnowwolf/packages' > feeds.conf.default
 echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
@@ -66,6 +38,7 @@ echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
 echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-21.02' >> feeds.conf.default
 echo 'src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-21.02' >> feeds.conf.default
 
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
