4bf5c94f2020680f4fb7b2093b2def00b13a9ed5
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-15 22:02:54 +0800

    immortalwrt扩展库

--- Scripts/quick.sh
+++ Scripts/quick.sh
@@ -42,6 +42,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/homeproxy
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-homeproxy
 #######################################################################################
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede qca
@@ -49,8 +51,9 @@ git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 #git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
 #git_sparse_clone main https://github.com/kenzok8/small-package ariang
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede zerotier
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-zerotier
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2dat
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
