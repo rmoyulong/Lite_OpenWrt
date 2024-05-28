d80d0e42e9f24f5b3c5536405e8d5d63a3544b39
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-29 02:32:00 +0800

    增加和删除扩展源

--- Scripts/remove.sh
+++ Scripts/remove.sh
@@ -5,3 +5,18 @@ rm -rf feeds/packages/net/homeproxy
 rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/kenzo/homeproxy
 rm -rf feeds/kenzo/luci-app-homeproxy
+####################################################
+#解决shadowsocks-libev编译错误
+git clone https://github.com/rmoyulong/AX6-Actions_Lede rmoyulong
+rm -rf feeds/packages/net/shadowsocks-libev
+cp -rf ./rmoyulong/shadowsocks-libev feeds/packages/net/
+
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
+cp -rf ./rmoyulong/libs/mbedtls package/libs/
+cp -rf ./rmoyulong/libs/ustream-ssl package/libs/
+cp -rf ./rmoyulong/libs/uclient package/libs/
+
+rm -rf rmoyulong
+#####################################################
