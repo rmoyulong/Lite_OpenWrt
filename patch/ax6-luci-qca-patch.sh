6802f5abe070e9c70171e52cdc92eee7d0867393
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-14 22:58:52 +0800

    ax6 luci 去lede-qca 补丁

--- patch/ax6-luci-qca-patch.sh
+++ patch/ax6-luci-qca-patch.sh
@@ -3,6 +3,6 @@ rm -rf openwrt/package/qca
 
 git config --global user.name "OpenWrt Builder"
 git config --global user.email "buster-openwrt@ovvo.uk"
-cp $GITHUB_WORKSPACE/patch/0001-show-soc-status-on-luci.patch openwrt/feeds/luci
+cp -rf $GITHUB_WORKSPACE/patch/0001-show-soc-status-on-luci.patch openwrt/feeds/luci
 cd openwrt/feeds/luci
 git am 0001-show-soc-status-on-luci.patch
