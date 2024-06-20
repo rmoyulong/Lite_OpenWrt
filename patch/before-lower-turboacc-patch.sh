d0320b454ff0c8217ae0a7eb343c96e5040c79fe
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-21 07:25:58 +0800

    前置补丁：设置内核小于5.15版本的turboacc插件

--- patch/before-lower-turboacc-patch.sh
+++ patch/before-lower-turboacc-patch.sh
@@ -5,7 +5,7 @@ source $GITHUB_WORKSPACE/Scripts/functions.sh
 #如果使用自定义的nss wifi插件或者不使用nss就要删除lede自带的qca
 rm -rf openwrt/package/qca
 
-#小于内核小于5.15版本，必须删除高版本的turboacc及依赖
+#内核小于5.15版本，必须删除高版本的turboacc及依赖
 rm -rf openwrt/package/luci-app-turboacc
 rm -rf openwrt/package/network/utils/fullconenat-nft
 rm -rf openwrt/package/network/utils/fullconenat
