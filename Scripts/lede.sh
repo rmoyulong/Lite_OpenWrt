fb4fd677b7287705a07c2679a49098f37b874c87
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-21 15:47:18 +0800

    自定义插件扩展源-- lede (all&k3专用)

--- Scripts/lede.sh
+++ Scripts/lede.sh
@@ -4,7 +4,7 @@ rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/lede.tar.gz
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/lede.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
 # 取消主题默认设置
--- Scripts/lede_k3.sh
+++ Scripts/lede_k3.sh
@@ -4,7 +4,7 @@ rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/lede.tar.gz
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/lede.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
 #lede
