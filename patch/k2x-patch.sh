b2aa7650efea98b70dae03b74f5a763c8e695fe7
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-26 03:04:55 +0800

    斐讯k2x 补丁

--- patch/k2x-patch.sh
+++ patch/k2x-patch.sh
@@ -2,3 +2,6 @@ cd openwrt
 
 cp -rf $GITHUB_WORKSPACE/patch/k2x/mt7620a_phicomm_psg1218a.dts target/linux/ramips/dts
 cp -rf $GITHUB_WORKSPACE/patch/k2x/mt7620.mk target/linux/ramips/image
+
+cp -rf $GITHUB_WORKSPACE/patch/k2x/package.tar.gz .
+tar zxvfp package.tar.gz
