50839b3c622e3b79ae9dec512346da75c40d7440
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-26 02:33:21 +0800

    添加immortalwrt 通用&k3 架构的自定义扩展源

--- Scripts/immortalwrt.sh
+++ Scripts/immortalwrt.sh
@@ -4,5 +4,5 @@ rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/immortalwrt.tar.gz
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
