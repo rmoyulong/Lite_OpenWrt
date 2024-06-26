42d16d379418173bbad00507583e69e2c163bba5
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-26 12:56:55 +0800

    Update K2.sh

--- Scripts/K2.sh
+++ Scripts/K2.sh
@@ -2,4 +2,4 @@
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
 rm -rf package/kernel/cryptodev-linux
-merge_package openwrt-23.05 https://github.com/openwrt/openwrt package package/kernel/cryptodev-linux
+merge_package openwrt-22.03 https://github.com/openwrt/openwrt package package/kernel/cryptodev-linux
