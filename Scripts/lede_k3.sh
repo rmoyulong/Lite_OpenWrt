a825c5ab656a6d265ed7f887783594d2b7fd5164
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-07-16 14:25:57 +0800

    自定义插件扩展源-- lede (k3专用)

--- Scripts/lede_k3.sh
+++ Scripts/lede_k3.sh
@@ -2,7 +2,7 @@
 
 # 回退源码2024-07-01
 git reset --hard 03835617b9b73e9b3fe0c600d957be1eefc212e1
-#git checkout -b 0383561
+git checkout -b 0383561
 
 rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
