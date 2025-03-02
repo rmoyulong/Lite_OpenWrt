5442867e3a06a0ba6b9768dae7294d058a2e6e57
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-03-02 16:58:12 +0800

    Delete remove/k2_remove.sh

--- remove/k2_remove.sh
+++
@@ -1,6 +0,0 @@
-rm -rf  feeds/helloworld/shadowsocks-rust
-####################################################
-#解决shadowsocks-libev编译错误
-rm -rf feeds/packages/net/shadowsocks-libev
-cp -rf ./package/shadowsocks-libev feeds/packages/net/
-#####################################################
