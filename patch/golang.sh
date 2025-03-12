eb23366bbd44895bef841b0a6e29500b382c9a03
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-03-12 17:50:50 +0800

    Delete patch/golang.sh

--- patch/golang.sh
+++
@@ -1,6 +0,0 @@
-#公用函数
-source $GITHUB_WORKSPACE/Scripts/functions.sh
-
-rm -rf feeds/packages/golang
-rm -rf feeds/packages/lang/golang
-merge_package master https://github.com/immortalwrt/packages package lang/golang
