8fac6312540178a172fb6cd3eacdba873a34036c
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-03-12 22:05:36 +0800

    old rust

---
+++ patch/old_rust.sh
@@ -0,0 +1,8 @@
+#公用函数
+source $GITHUB_WORKSPACE/Scripts/functions.sh
+
+cd openwrt
+#####################################################
+rm -rf feeds/packages/lang/rust
+merge_package master https://github.com/coolsnowwolf/packages package lang/rust
+#####################################################
