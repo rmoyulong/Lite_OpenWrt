fc6d0d2c653be668573f367601997a180ea8b1a4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-03-12 10:47:07 +0800

    golang

--- patch/golang.sh
+++ patch/golang.sh
@@ -1,3 +1,6 @@
+#公用函数
+source $GITHUB_WORKSPACE/Scripts/functions.sh
+
 rm -rf feeds/packages/golang
-rm -rf feeds/packages/lang/golang/golang
-apt install -y golang
+rm -rf feeds/packages/lang/golang
+merge_package master https://github.com/immortalwrt/packages package lang/golang
