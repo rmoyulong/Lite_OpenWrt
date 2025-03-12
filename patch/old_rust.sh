fdead2c410cf490b26c24dc150d5c78e2c18ade6
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-03-13 00:43:43 +0800

    old rust

--- patch/old_rust.sh
+++ patch/old_rust.sh
@@ -4,5 +4,5 @@ source $GITHUB_WORKSPACE/Scripts/functions.sh
 cd openwrt
 #####################################################
 rm -rf feeds/packages/lang/rust
-merge_package master https://github.com/coolsnowwolf/packages feeds/packages lang/rust
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede rust
 #####################################################
