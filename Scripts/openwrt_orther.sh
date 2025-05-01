6778c3bda7dd69a2c4962762a94d6b635196317e
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-01 18:45:26 +0800

    正宗immortalwrt homeproxy插件源

--- Scripts/openwrt_orther.sh
+++ Scripts/openwrt_orther.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
+rm -rf package/network/services/hostapd
 rm -rf feeds/packages/net/curl
 
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/openwrt_orther.tar.gz
 tar zxvfp rmoyulong.tar.gz
-
-#修改qca-nss-drv启动顺序
-NSS_DRV="feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-if [ -f "$NSS_DRV" ]; then
-	sed -i 's/START=.*/START=85/g' $NSS_DRV
-
-	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-fi
-
-#修改qca-nss-pbuf启动顺序
-NSS_PBUF="package/kernel/mac80211/files/qca-nss-pbuf.init"
-if [ -f "$NSS_PBUF" ]; then
-	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-
-	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-fi
