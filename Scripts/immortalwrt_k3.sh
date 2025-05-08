5ded5b545c98c734932b8e983dfaabaefbae87b4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-08 18:02:18 +0800

    K3 immortalwrt 正常版(ssr homeproxy)

--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -7,19 +7,24 @@ rm -rf package/libs/uclient
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
-
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/yangxu52/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
-echo '=========Add k3screen plug OK!========='
+git clone https://github.com/li1507/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+echo '========= K3屏幕插件 加载完成！ ========='
 
 echo '替换lwz322的K3屏幕驱动插件'
 rm -rf package/lean/k3screenctrl
-git clone https://github.com/yangxu52/k3screenctrl_build.git package/lean/k3screenctrl/
-echo '=========Replace k3screen drive plug OK!========='
+git clone https://github.com/li1507/k3screenctrl_build.git package/lean/k3screenctrl/
+echo '========= K3屏幕驱动插件 加载完成！ ========='
 
-FIRMWARE=69027
+echo '移除bcm53xx中的其他机型'
+sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+echo '========= 移除bcm53xx中的其他机型完成！ ========='
+
+# 1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
+firmware='69027'
 echo '替换无线驱动'
-echo 'https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${{FIRMWARE}}'
-wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-echo '=========Replace k3 wireless firmware OK!========='
+wget -nv https://github.com/li1507/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+echo '========= 替换无线驱动替换完成！ ========='
