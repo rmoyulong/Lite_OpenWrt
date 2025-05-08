bc6dd4c8f34c695141fcfe165a1567a5c0e668e4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-08 18:20:39 +0800

    K3 immortalwrt 正常版(ssr homeproxy)

--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -17,13 +17,6 @@ rm -rf package/lean/k3screenctrl
 git clone https://github.com/li1507/k3screenctrl_build.git package/lean/k3screenctrl/
 echo '========= K3屏幕驱动插件 加载完成！ ========='
 
-echo '移除bcm53xx中的其他机型'
-sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
-echo '========= 移除bcm53xx中的其他机型完成！ ========='
-
-# 1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
 firmware='69027'
 echo '替换无线驱动'
 wget -nv https://github.com/li1507/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
