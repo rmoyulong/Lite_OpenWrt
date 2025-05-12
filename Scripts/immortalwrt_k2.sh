e72939cb2d910740ffefc3bdb3575347147abaf6
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-13 06:42:21 +0800

    Update immortalwrt_k2.sh

--- Scripts/immortalwrt_k2.sh
+++ Scripts/immortalwrt_k2.sh
@@ -1,1 +1,8 @@
-dd
+#!/bin/bash
+
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
+
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt_k2.tar.gz
+tar zxvfp rmoyulong.tar.gz
