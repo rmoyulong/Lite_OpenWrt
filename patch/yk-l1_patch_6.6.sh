d4e3ce8c78b9eef414eef0f24a76ac18e3d71105
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-13 15:14:22 +0800

    优酷路由宝 lede 6.6补丁

--- patch/yk-l1_patch_6.6.sh
+++ patch/yk-l1_patch_6.6.sh
@@ -1,9 +1,11 @@
 cd openwrt
        
-#修改优酷路由宝l1的的内核版本
-rm -rf target/linux/ramips
+#修改优酷路由宝l1 lede 版本的内核版本
+#rm -rf target/linux/ramips
 git clone -q --single-branch --depth=1 --branch=master https://github.com/immortalwrt/immortalwrt immortalwrt
 
-mv ./immortalwrt/target/linux/ramips target/linux
+mv $GITHUB_WORKSPACE/patch/ramips/6.6/Makefile target/linux/ramips
+mv ./immortalwrt/target/linux/ramips/patches-6.6 target/linux/ramips
+mv ./immortalwrt/target/linux/ramips/mt7620/config-6.6 target/linux/ramips/mt7620
 #chmod -Rf 755 target/linux/ramips
 ls -l target/linux/ramips         
