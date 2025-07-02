7fbe294968959ae85e0ecbcfac8eeb0c26986e73
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-07-03 04:24:14 +0800

    k3 lede patch

--- patch/python.patch_lede.sh
+++ patch/python.patch_lede.sh
@@ -2,9 +2,9 @@
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
 cd openwrt
-rm -rf feeds/packages/lang/python
+#rm -rf feeds/packages/lang/python
 
-merge_package master https://github.com/coolsnowwolf/packages feeds/packages/lang lang/python
+#merge_package master https://github.com/coolsnowwolf/packages feeds/packages/lang lang/python
 
 git clone -q --single-branch --depth=1 --branch=master https://github.com/immortalwrt/immortalwrt immortalwrt
 rm -rf target/linux/bcm53xx
