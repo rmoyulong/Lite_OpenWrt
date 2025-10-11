4b20ddc91d69934ba5ba826e982821193ed593fe
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-10-11 20:57:13 +0800

    优酷路由宝 补丁

--- patch/yk-l1_patch.sh
+++ patch/yk-l1_patch.sh
@@ -1,10 +1,12 @@
+#公用函数
+#source $GITHUB_WORKSPACE/Scripts/functions.sh
+rm -rf feeds/packages/lang/python
+
+merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
 cd openwrt
        
 #修改优酷路由宝l1的的内核版本
 mv $GITHUB_WORKSPACE/patch/ramips/Makefile target/linux/ramips
 #chmod -Rf 755 target/linux/ramips
 ls -l target/linux/ramips 
-
-rm -rf feeds/packages/lang/python
-
-merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
