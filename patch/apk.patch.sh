2fba7ade4dec9540cbb2e33db2f4dc76e484de25
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-01-16 09:50:45 +0800

    Delete patch/apk.patch.sh

--- patch/apk.patch.sh
+++
@@ -1,7 +0,0 @@
-#公用函数
-source $GITHUB_WORKSPACE/Scripts/functions.sh
-
-cd openwrt
-rm -rf package/system/apk
-
-merge_package master https://github.com/immortalwrt/immortalwrt package/system/apk package/system/apk
