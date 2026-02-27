8a11c5f7fd2b7e23b26dd3e2dc1a0fba94d5ca52
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-02-27 21:20:44 +0800

    old python3

--- patch/python.patch.sh
+++ patch/python.patch.sh
@@ -5,3 +5,6 @@ cd openwrt
 rm -rf feeds/packages/lang/python
 
 merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
+#rm libs/mbedtls/Makefile
+cp -f $GITHUB_WORKSPACE/patch/mbedtls/Makefile package/libs/mbedtls/Makefile
