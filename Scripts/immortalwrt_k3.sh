8ca64cc7092bc990ff5d2f038cd2c32a4c4b2fbb
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 20:54:02 +0800

    自定义插件设置

--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -25,11 +25,11 @@ echo '========= 替换无线驱动替换完成！ ========='
 #公用函数
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
-rm -rf feeds/packages/lang/rust
 merge_package master https://github.com/coolsnowwolf/packages package lang/rust
-
 git clone https://github.com/stupidloud/helloworld package/helloworld
-rm -rf feeds/packages/net/xray-core
+
+rm -rf package/helloworld/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
