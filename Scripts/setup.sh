e9d0849ece1658f2998e9fc1447d94a5ec8ea065
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 15:12:58 +0800

    自定义插件设置

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,12 +1,13 @@
 #公用函数
-#source $GITHUB_WORKSPACE/Scripts/functions.sh
+source $GITHUB_WORKSPACE/Scripts/functions.sh
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git kenzo  https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 
 #rm libs/mbedtls/Makefile
 #cp -f $GITHUB_WORKSPACE/patch/mbedtls/Makefile package/libs/mbedtls/Makefile
+git_sparse_clone master https://github.com/kenzok8/small luci-app-ssr-plus
 
 cd package
-$GITHUB_WORKSPACE/patch/Packages.sh
-$GITHUB_WORKSPACE/patch/Handles.sh
+$GITHUB_WORKSPACE/sh/Packages.sh
+$GITHUB_WORKSPACE/sh/Handles.sh
