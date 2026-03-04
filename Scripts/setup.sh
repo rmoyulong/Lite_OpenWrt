e561a2b4565ccd8fe1232b680435de11fb2f368d
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 17:13:50 +0800

    自定义插件打包

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,12 +1,11 @@
 #公用函数
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-#echo 'src-git kenzo  https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
+rm -rf feeds/packages/lang/rust
+merge_package master https://github.com/coolsnowwolf/packages package lang/rust
 
-#rm libs/mbedtls/Makefile
-#cp -f $GITHUB_WORKSPACE/patch/mbedtls/Makefile package/libs/mbedtls/Makefile
-git_sparse_clone master https://github.com/kenzok8/small luci-app-ssr-plus
+git clone https://github.com/kenzok8/small package/kenzok8
+rm -rf package/kenzok8/xray-core
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
