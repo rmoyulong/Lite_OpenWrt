2018c97fc1292e725089b700d15ce0b94a1dedea
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 19:02:24 +0800

    自定义插件设置

--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -28,8 +28,8 @@ source $GITHUB_WORKSPACE/Scripts/functions.sh
 rm -rf feeds/packages/lang/rust
 merge_package master https://github.com/coolsnowwolf/packages package lang/rust
 
-git clone https://github.com/kenzok8/small package/kenzok8
-rm -rf package/kenzok8/xray-core
+git clone https://github.com/stupidloud/helloworld package/helloworld
+rm -rf feeds/packages/net/xray-core
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
