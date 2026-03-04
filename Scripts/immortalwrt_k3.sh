2b298a04996930d1a5c3e0a7829119397b4604ff
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-04 12:41:46 +0800

    K3 immortalwrt 正常版(ssr homeproxy)

--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf package/libs/mbedtls
-rm -rf package/libs/ustream-ssl
-rm -rf package/libs/uclient
+#rm -rf package/libs/mbedtls
+#rm -rf package/libs/ustream-ssl
+#rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
-tar zxvfp rmoyulong.tar.gz
+#sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
+#tar zxvfp rmoyulong.tar.gz
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
@@ -21,3 +21,7 @@ firmware='69027'
 echo '替换无线驱动'
 wget -nv https://github.com/li1507/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '========= 替换无线驱动替换完成！ ========='
+
+cd package
+$GITHUB_WORKSPACE/patch/Packages.sh
+$GITHUB_WORKSPACE/patch/Handles.sh
