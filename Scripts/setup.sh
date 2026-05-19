9a3c51bb12333f5f5507292c58c98b03dedebea0
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 19:46:25 +0800

    更新到最新插件

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -7,10 +7,10 @@ git clone https://github.com/VIKINGYFY/homeproxy package/homeproxy
 rm -rf feeds/luci/applications/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 
-git clone  https://github.com/rmoyulong/helloworld package/helloworld
-rm -rf package/passwall-packages/geoview
-rm -rf package/passwall-packages/hysteria
-rm -rf package/passwall-packages/xray-core
+git clone https://github.com/stupidloud/helloworld package/helloworld
+rm -rf package/helloworld/geoview
+rm -rf package/helloworld/hysteria
+rm -rf package/helloworld/xray-core
 
 git_sparse_clone master https://github.com/kenzok8/small sing-box
 
