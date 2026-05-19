2877ef04794b1f8f6dff6764fa24f251afc0378e
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 15:13:27 +0800

    更新到最新插件

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -9,6 +9,8 @@ git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 git_sparse_clone main https://github.com/kiddin9/op-packages luci-app-ssr-plus
+git_sparse_clone main https://github.com/kiddin9/op-packages dns2socks-rust
+git_sparse_clone main https://github.com/kiddin9/op-packages kcptun-client
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
