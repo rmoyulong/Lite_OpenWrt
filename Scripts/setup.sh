6824d3f9bb2f21e32cd9225156aaccb8773ba6c1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 16:44:23 +0800

    更新到最新插件

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -10,7 +10,9 @@ git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 git_sparse_clone main https://github.com/kiddin9/op-packages luci-app-ssr-plus
 git_sparse_clone main https://github.com/kiddin9/op-packages dns2socks-rust
-git_sparse_clone main https://github.com/kiddin9/op-packages kcptun-client
+
+git clone  https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+rm -rf package/passwall-packages/geoview
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
