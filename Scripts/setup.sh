968eae908f785901b848ca6e637f22998c559be2
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 19:29:18 +0800

    更新到最新插件

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -7,13 +7,12 @@ git clone https://github.com/VIKINGYFY/homeproxy package/homeproxy
 rm -rf feeds/luci/applications/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-git_sparse_clone main https://github.com/kiddin9/op-packages luci-app-ssr-plus
-git_sparse_clone main https://github.com/kiddin9/op-packages dns2socks-rust
-
-git clone  https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+git clone  https://github.com/rmoyulong/helloworld package/helloworld
 rm -rf package/passwall-packages/geoview
 rm -rf package/passwall-packages/hysteria
+rm -rf package/passwall-packages/xray-core
+
+git_sparse_clone master https://github.com/kenzok8/small sing-box
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
