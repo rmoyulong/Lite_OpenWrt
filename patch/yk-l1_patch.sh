c7e39d13b48f287e9364aa16a4f861c06e32219c
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 19:42:01 +0800

    优酷路由宝 lede补丁

--- patch/yk-l1_patch.sh
+++ patch/yk-l1_patch.sh
@@ -3,10 +3,8 @@ source $GITHUB_WORKSPACE/Scripts/functions.sh
 
 cd openwrt
 
-rm -rf feeds/packages/net/nmap/*
-git_sparse_clone master https://github.com/immortalwrt/packages /net/nmap
-cp -rf package/nmap/* feeds/packages/net/nmap
-rm -rf package/nmap
+rm -rf feeds/packages/net/nmap
+merge_package master https://github.com/immortalwrt/packages feeds/packages/net net/nmap
 
 #修改优酷路由宝l1的的内核版本
 mv $GITHUB_WORKSPACE/patch/ramips/Makefile target/linux/ramips
