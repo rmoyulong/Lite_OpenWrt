c1e8e7736bd49da1295648b74e7f6e0afaad6994
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 16:44:47 +0800

    Delete remove/new_remove.sh

--- remove/new_remove.sh
+++
@@ -1,5 +0,0 @@
-rm -rf feeds/packages/lang/rust
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/sing-box
-git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
