1516e7005f88aaf745ec0f8269b9a7ee36a0fac2
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 16:45:39 +0800

    Delete patch/old_rust.sh

--- patch/old_rust.sh
+++
@@ -1,8 +0,0 @@
-#公用函数
-source $GITHUB_WORKSPACE/Scripts/functions.sh
-
-cd openwrt
-#####################################################
-rm -rf feeds/packages/lang/rust
-git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede rust
-#####################################################
