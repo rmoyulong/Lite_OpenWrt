8ecfcaad8c5d513028811b973df23cb15bd0dca4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 15:29:01 +0800

    更新到最新插件 && 通用设置

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -8,27 +8,26 @@ rm -rf feeds/luci/applications/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 
 rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/packages/net/geoview/*
 rm -rf feeds/packages/net/hysteria/*
-rm -rf feeds/packages/net/v2ray-core/*
 rm -rf feeds/packages/net/xray-core/*
+rm -rf feeds/packages/net/v2ray-core/*
 rm -rf feeds/packages/net/v2ray-geodata/*
 git clone https://github.com/stupidloud/helloworld package/helloworld
-#rm -rf package/helloworld/geoview
+cp -rf package/helloworld/hysteria/* feeds/packages/net/hysteria
+cp -rf package/helloworld/xray-core/* feeds/packages/net/xray-core
+cp -rf package/helloworld/v2ray-core/* feeds/packages/net/v2ray-core
+cp -rf package/helloworld/v2ray-geodata/* feeds/packages/net/v2ray-geodata
 #rm -rf package/helloworld/hysteria
 #rm -rf package/helloworld/xray-core
+#rm -rf package/helloworld/v2ray-core
 #rm -rf package/helloworld/v2ray-geodata 
-cp -rf package/helloworld/geoview/* feeds/packages/net/geoview
-cp -rf package/helloworld/hysteria/* feeds/packages/net/hysteria
-cp -rf package/helloworld/v2ray-core/* feeds/packages/net/v2ray-core
-cp -rf package/helloworld/xray-core/* feeds/packages/net/xray-core
-cp -rf package/helloworld/v2ray-geodata/* feeds/packages/net/v2ray-geodata
 
 rm -rf feeds/packages/net/sing-box/*
 #mkdir -p feeds/packages/net/sing-box
 git_sparse_clone main https://github.com/kiddin9/op-packages sing-box
 cp -rf package/sing-box/* feeds/packages/net/sing-box
 rm -rf package/sing-box
+
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
 $GITHUB_WORKSPACE/Scripts/Handles.sh
