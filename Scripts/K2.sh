30689e700470c013e5253fec13ea77511d0c8550
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-08-18 18:12:38 +0800

    Add files via upload

--- Scripts/K2.sh
+++ Scripts/K2.sh
@@ -9,7 +9,7 @@
 #公用函数
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
-echo 'src-git helloworld https://github.com/stupidloud/helloworld' >> feeds.conf.default
+#echo 'src-git helloworld https://github.com/stupidloud/helloworld' >> feeds.conf.default
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
 
 #git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
@@ -27,7 +27,22 @@ echo 'src-git helloworld https://github.com/stupidloud/helloworld' >> feeds.conf
 
 #dos2unix
 merge_package master https://github.com/immortalwrt/packages package utils/dos2unix
-git_sparse_clone master https://github.com/kenzok8/small luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+git clone https://github.com/stupidloud/helloworld package/helloworld
+
+rm -rf feeds/packages/net/hysteria/*
+rm -rf feeds/packages/net/xray-core/*
+rm -rf feeds/packages/net/v2ray-core/*
+cp -rf package/helloworld/hysteria/* feeds/packages/net/hysteria
+cp -rf package/helloworld/xray-core/* feeds/packages/net/xray-core
+cp -rf package/helloworld/v2ray-core/* feeds/packages/net/v2ray-core
+
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/helloworld/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+rm -rf feeds/packages/net/sing-box
+merge_package main https://github.com/VIKINGYFY/packages  feeds/packages/net sing-box
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
--- Scripts/immortalwrt_k3.sh
+++ Scripts/immortalwrt_k3.sh
@@ -25,20 +25,22 @@ echo '========= 替换无线驱动替换完成！ ========='
 #公用函数
 source $GITHUB_WORKSPACE/Scripts/functions.sh
 
-merge_package master https://github.com/coolsnowwolf/packages package lang/rust
+rm -rf feeds/luci/applications/luci-app-ssr-plus
 git clone https://github.com/stupidloud/helloworld package/helloworld
 
+rm -rf feeds/packages/net/hysteria/*
+rm -rf feeds/packages/net/xray-core/*
+rm -rf feeds/packages/net/v2ray-core/*
+cp -rf package/helloworld/hysteria/* feeds/packages/net/hysteria
+cp -rf package/helloworld/xray-core/* feeds/packages/net/xray-core
+cp -rf package/helloworld/v2ray-core/* feeds/packages/net/v2ray-core
+
+rm -rf feeds/packages/net/v2ray-geodata
 rm -rf package/helloworld/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-
-rm -rf package/helloworld/shadowsocks-rust
-merge_package master https://github.com/kenzok8/small/ package shadowsocks-rust
-
-rm -rf package/helloworld/xray-core
-git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall-packages xray-core
-
-git_sparse_clone master https://github.com/kenzok8/small sing-box
+rm -rf feeds/packages/net/sing-box
+merge_package main https://github.com/VIKINGYFY/packages  feeds/packages/net sing-box
 
 cd package
 $GITHUB_WORKSPACE/Scripts/Packages.sh
